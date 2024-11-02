9cf67480fb4f4d8c8ef0504ce0114888c22ea322
John Ron Diza <202210388@gordoncollege.edu.ph>
2024-11-02 18:21:38 +0800

    Small Changes for Links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,7 +34,7 @@
         </div>
         
         <a 
-            href="#homePage" 
+            href="/" 
             target="_self"
             class="relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
         >
